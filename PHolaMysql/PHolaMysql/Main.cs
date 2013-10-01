f2903ac945e2ed11be4f8db7c9e654b0cd9d3223
nerea123 <sweetlolipop@hotmail.es>
2013-10-01 10:17:22 +0200

    ,odificacion de metodos con IEnumerable

--- PHolaMysql/PHolaMysql/Main.cs
+++ PHolaMysql/PHolaMysql/Main.cs
@@ -1,11 +1,20 @@
 using System;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace Serpis.Ad
 {
 	class MainClass
 	{
-		private static string[] getcolumNames(MySqlDataReader mysqlDataReader){
+		private static IEnumerable<string> getcolumNames2(MySqlDataReader mysqlDataReader){
+			int numCol=mysqlDataReader.FieldCount;
+			List<string> columNames= new List<string>();
+			for(int i=0;i<numCol;i++)
+				columNames.Add(mysqlDataReader.GetName(i));
+			return columNames;
+		}
+		
+		private static IEnumerable<string> getcolumNames(MySqlDataReader mysqlDataReader){
 			string[] names=new string [mysqlDataReader.FieldCount];
 			int numCol=mysqlDataReader.FieldCount;
 			for(int i=0;i<numCol;i++){
@@ -43,14 +52,17 @@ namespace Serpis.Ad
 			    
 			Console.WriteLine();
 			//obtener cabeceras mediante metodo
-			string[] names=getcolumNames(mysqlDataReader);
+			string[] names=(string[])getcolumNames(mysqlDataReader);
 			
 			for(int i=0;i<names.Length;i++)
 				Console.Write(names[i]+"   ");
 			
+			Console.WriteLine();
+			Console.WriteLine(string.Join("  ",getcolumNames(mysqlDataReader)));
+			
 			mysqlDataReader.Close();
 			mySqlConnection.Close();
 			Console.WriteLine ("ok");
-		}
+		}	
 	}
 }
